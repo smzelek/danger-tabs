d244c6888a776ca2b12456457baf9de60e5140a0
Steven Zelek <11831150+smzelek@users.noreply.github.com>
2025-08-21 12:44:37 -0400

    feat: works for real now

--- background.js
+++ background.js
@@ -1,95 +1,90 @@
-chrome.webNavigation.onCompleted.addListener(info => {
-    const url = info.url
-
-    if (url.includes('chrome://') || url.includes('about:blank')) {
-        return;
+/** @type {Options} */
+const defaultOptions = {
+  enabled: true,
+  config: [
+    {
+      urls: ["https://www.google.com"],
+      color: "red",
+      label: "Production!"
     }
+  ]
+};
 
-    chrome.scripting.executeScript({
-        target: {
-            tabId: info.tabId,
-            documentIds: [info.documentId]
-        },
-        func: tabWarning,
-        args: [url]
-    });
+chrome.runtime.onInstalled.addListener(details => {
+  if (details.reason === "install") {
+    chrome.storage.sync.set(
+      {
+        options: defaultOptions
+      },
+      () => { }
+    );
+  }
 });
 
-function tabWarning(url) {
-    setTimeout(() => {
-        chrome.storage.sync.get(
-            "disabled",
-            ({ disabled }) => {
-                setTimeout(() => tabWarning(url), 3000);
-                document.querySelectorAll('#danger-tabs-warning').forEach(el => el.remove());
-                // document.body.style.boxShadow = '';
+chrome.webNavigation.onCompleted.addListener(info => {
+  chrome.scripting.executeScript({
+    target: {
+      tabId: info.tabId,
+      documentIds: [info.documentId]
+    },
+    func: (/** @type {any} */ (run)),
+    args: [info.url]
+  });
+});
 
-                if (disabled) {
-                    return;
-                }
+/**
+ * @param {string} url
+ * @returns
+ */
+function run(url) {
+  if (url.match('chrome://') || url.match('about:blank')) {
+    return;
+  }
 
-                const isProdBuilder = url.includes("https://builder.io") || url.includes("https://www.builder.io");
-                const isLocalBuilder = url.includes("local.builder.io") || url.includes("localhost:1234");
-                const isLocalAiAssistant = url.includes("localhost:7242");
-                const isProdAiAssistant = url.includes("https://assistant.builder.io");
-                const isRootOrg = document.body.innerHTML.includes("d3e7739f05c5462bad48687394709cb2");
-                const isAdminTestOrg = document.body.innerHTML.includes("d3932a87091340008e251024533e0207");
-                const isPersonalOrg = document.body.innerHTML.includes("c49bd068972749829e2f2ccc01b930ea");
+  const _run = () => chrome.storage.sync.get(
+    "options",
+    /**
+     * @param {{options: Options}} _
+     * @returns
+     */
+    ({ options }) => {
+      const { enabled, config } = options ?? defaultOptions;
+      document.querySelectorAll('#danger-tabs-warning').forEach(el => el.remove());
 
-                if ([isProdBuilder, isLocalBuilder, isLocalAiAssistant, isProdAiAssistant, isRootOrg, isAdminTestOrg, isPersonalOrg].every(x => x === false)) {
-                    return;
-                }
+      if (!enabled) {
+        return;
+      }
 
-                const label = [
-                    isLocalBuilder && 'local',
-                    isLocalAiAssistant && 'local',
-                    isProdAiAssistant && 'PROD',
-                    isProdBuilder && 'PROD',
-                    isRootOrg && 'BUILDER',
-                    isAdminTestOrg && 'test',
-                    isPersonalOrg && 'personal',
-                ].filter(s => typeof (s) === 'string').join(',');
+      const matches = config.filter(sc => sc.urls.some(scu => url.match(scu)));
 
-                const color = (() => {
-                    if (isRootOrg) {
-                        return 'red';
-                    }
-                    if (isProdBuilder) {
-                        return 'red';
-                    }
-                    if (isAdminTestOrg) {
-                        return 'green';
-                    }
-                    if (isPersonalOrg) {
-                        return 'green';
-                    }
-                    if (isLocalBuilder) {
-                        return '#2d4695';
-                    }
-                    if (isLocalAiAssistant) {
-                        return '#2d4695';
-                    }
-                    if (isProdAiAssistant) {
-                        return 'red';
-                    }
-                })();
+      if (matches.length === 0) {
+        return;
+      }
 
-                // document.body.style.boxShadow = `0 0 0 5px ${color}`;
-                let dangerTabsWarning = document.createElement('span');
-                dangerTabsWarning.innerText = `<${label}>`;
-                dangerTabsWarning.id = "danger-tabs-warning"
-                dangerTabsWarning.style.backgroundColor = color;
-                dangerTabsWarning.style.color = 'white';
-                dangerTabsWarning.style.fontWeight = 'bold'
-                dangerTabsWarning.style.fontSize = '10px';
-                dangerTabsWarning.style.lineHeight = '10px';
-                dangerTabsWarning.style.fontFamily = 'monospace';
-                dangerTabsWarning.style.position = 'fixed';
-                dangerTabsWarning.style.top = '0px';
-                dangerTabsWarning.style.left = '0px';
-                dangerTabsWarning.style.zIndex = '100';
-                document.body.insertBefore(dangerTabsWarning, document.body.firstChild);
-            }
-        );
-    }, 1500)
+      const label = matches.map(m => m.label).join(',');
+      const color = matches[0].color;
+
+      const dangerTabsWarning = document.createElement('span');
+      dangerTabsWarning.innerText = `<${label}>`;
+      dangerTabsWarning.id = "danger-tabs-warning"
+      dangerTabsWarning.style.backgroundColor = color;
+      dangerTabsWarning.style.color = 'white';
+      dangerTabsWarning.style.fontWeight = 'bold'
+      dangerTabsWarning.style.fontSize = '10px';
+      dangerTabsWarning.style.lineHeight = '10px';
+      dangerTabsWarning.style.fontFamily = 'monospace';
+      dangerTabsWarning.style.position = 'fixed';
+      dangerTabsWarning.style.top = '0px';
+      dangerTabsWarning.style.left = '0px';
+      dangerTabsWarning.style.zIndex = '999999999999999';
+
+      document.body.insertBefore(dangerTabsWarning, document.body.firstChild);
+    }
+  );
+
+  chrome.storage.sync.onChanged.addListener(() => {
+    _run();
+  })
+
+  _run();
 }
--- popup.js
+++ popup.js
@@ -1,25 +1,52 @@
-const saveOptions = () => {
-    const disabled = document.getElementById('disabled').checked;
+/** 
+ * @typedef {object} Options
+ * @property {Config} config
+ * @property {boolean} enabled
+*/
 
-    chrome.storage.sync.set(
-        { disabled },
-        () => {
-            const status = document.getElementById('status');
-            status.textContent = 'Options saved.';
-            setTimeout(() => {
-                status.textContent = '';
-            }, 750);
-        }
-    );
+/**
+ * @typedef {SiteConfig[]} Config
+ */
+
+/**
+ * @typedef {object} SiteConfig
+ * @property {string[]}   urls
+ * @property {string}     color
+ * @property {string}     label
+ */
+
+const saveOptions = () => {
+  const enabled = (/** @type {HTMLInputElement} */ (document.getElementById('enabled'))).checked;
+  const config = (/** @type {HTMLTextAreaElement} */  (document.getElementById('config'))).value;
+
+  chrome.storage.sync.set(
+    {
+      options: {
+        enabled,
+        config: JSON.parse(config)
+      }
+    },
+    () => {
+      const status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(() => {
+        status.textContent = '';
+      }, 750);
+    }
+  );
 };
 
 const restoreOptions = () => {
-    chrome.storage.sync.get(
-        "disabled",
-        ({ disabled }) => {
-            document.getElementById('disabled').checked = disabled || false;
-        }
-    );
+  chrome.storage.sync.get(
+    "options",
+    /**
+     * @param {{ options: Options }} args
+     */
+    ({ options }) => {
+      (/** @type {HTMLInputElement} */ (document.getElementById('enabled'))).checked = options.enabled;
+      (/** @type {HTMLTextAreaElement} */ (document.getElementById('config'))).value = JSON.stringify(options.config, null, 2);
+    }
+  );
 };
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
